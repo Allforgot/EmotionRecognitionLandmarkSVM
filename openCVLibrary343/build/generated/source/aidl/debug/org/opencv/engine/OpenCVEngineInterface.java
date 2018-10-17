b5f2df6afa5ee88042dfb32bfe26e247dd5012c9
tanzhenxing <tanzx@emwit.com>
2018-10-17 22:15:17 +0800

    test

--- openCVLibrary343/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary343/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: G:\\FaceDetectDemo-master\\FaceDetectDemo-master\\openCVLibrary343\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: G:\\CompanyProject\\EmotionRecognition\\FaceDetectDemo-master\\FaceDetectDemo-master\\openCVLibrary343\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
